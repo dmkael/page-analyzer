2095998d33f1a36a6b953fa7cb69fd0a8e26a033
dmkael <tanmaar@gmail.com>
2024-04-22 16:48:33 +0300

    RF repo uses cursor_decorator for methods & in datetime call changed method to get utc current time

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 install:
 	poetry install
 dev:
-	poetry run flask --debug --app page_analyzer:app run --port 8000 --host 0.0.0.0
+	poetry run flask --debug --app page_analyzer:app run --port 8000 --host localhost
 PORT ?= 8000
 start:
 	poetry run gunicorn -w 5 -b 0.0.0.0:$(PORT) page_analyzer:app
--- page_analyzer/db_repository.py
+++ page_analyzer/db_repository.py
@@ -1,6 +1,16 @@
 from psycopg2.extras import NamedTupleCursor
-from datetime import datetime
+from datetime import datetime, timezone
 import psycopg2
+
+
+def cursor_decorator(func):
+    def wrapper(repo, *args):
+        conn = repo.connect()
+        with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
+            result = func(repo, cursor, *args) if args else func(repo, cursor)
+            conn.commit()
+            return result
+    return wrapper
 
 
 class UrlRepo:
@@ -11,89 +21,81 @@ class UrlRepo:
         connection = psycopg2.connect(self.database)
         return connection
 
-    def get_urls_data(self):
-        conn = self.connect()
-        with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
-            cursor.execute('''
-                SELECT
-                    urls.id AS id,
-                    urls.name AS name,
-                    url_checks.status_code AS status_code,
-                    url_checks.created_at AS last_check
-                FROM urls
-                LEFT JOIN url_checks ON urls.id = url_checks.url_id
-                WHERE url_checks.created_at IN (
-                    SELECT MAX(created_at)
-                    FROM url_checks
-                    GROUP BY url_id
-                    ) OR url_checks.created_at IS NULL
-                ORDER BY urls.created_at DESC''')
-            all_urls_data = cursor.fetchall()
-            return all_urls_data
-
-    def get_url_by_id(self, url_id):
-        conn = self.connect()
-        with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
-            cursor.execute('SELECT * FROM urls WHERE id=%s', (url_id,))
-            located_url = cursor.fetchone()
-            return located_url
-
-    def get_url_checks(self, url_id):
-        conn = self.connect()
-        with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
-            cursor.execute('''
-                SELECT
-                    id,
-                    status_code,
-                    h1,
-                    title,
-                    description,
-                    created_at
+    @cursor_decorator
+    def get_urls_data(self, cursor):
+        cursor.execute('''
+            SELECT
+                urls.id AS id,
+                urls.name AS name,
+                url_checks.status_code AS status_code,
+                url_checks.created_at AS last_check
+            FROM urls
+            LEFT JOIN url_checks ON urls.id = url_checks.url_id
+            WHERE url_checks.created_at IN (
+                SELECT MAX(created_at)
                 FROM url_checks
-                WHERE url_id = %s
-                ORDER BY created_at DESC''', (url_id,))
-            url_checks = cursor.fetchall()
-            return url_checks
+                GROUP BY url_id
+                ) OR url_checks.created_at IS NULL
+            ORDER BY urls.created_at DESC''')
+        all_urls_data = cursor.fetchall()
+        return all_urls_data
 
-    def get_url_by_name(self, url):
-        conn = self.connect()
-        with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
-            cursor.execute('SELECT * FROM urls WHERE name=%s', (url,))
-            located_url = cursor.fetchone()
-            return located_url
+    @cursor_decorator
+    def get_url_by_id(self, cursor, url_id):
+        cursor.execute('SELECT * FROM urls WHERE id=%s', (url_id,))
+        located_url = cursor.fetchone()
+        return located_url
 
-    def save_url(self, url):
-        conn = self.connect()
-        with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
-            cursor.execute(
-                '''INSERT INTO urls (name, created_at)
-                VALUES (%s, %s)
-                RETURNING id''',
-                (url, datetime.utcnow())
+    @cursor_decorator
+    def get_url_checks(self, cursor, url_id):
+        cursor.execute('''
+            SELECT
+                id,
+                status_code,
+                h1,
+                title,
+                description,
+                created_at
+            FROM url_checks
+            WHERE url_id = %s
+            ORDER BY created_at DESC''', (url_id,))
+        url_checks = cursor.fetchall()
+        return url_checks
+
+    @cursor_decorator
+    def get_url_by_name(self, cursor, url):
+        cursor.execute('SELECT * FROM urls WHERE name=%s', (url,))
+        located_url = cursor.fetchone()
+        return located_url
+
+    @cursor_decorator
+    def save_url(self, cursor, url):
+        cursor.execute(
+            '''INSERT INTO urls (name, created_at)
+            VALUES (%s, %s)
+            RETURNING id''',
+            (url, datetime.now(timezone.utc))
+        )
+        new_record = cursor.fetchone()
+        return new_record.id
+
+    @cursor_decorator
+    def save_url_check(self, cursor, url_id, tags_data, status_code):
+        cursor.execute(
+            '''INSERT INTO url_checks (
+                url_id,
+                h1,
+                title,
+                description,
+                status_code,
+                created_at)
+            VALUES (%s, %s, %s, %s, %s, %s)''',
+            (
+                url_id,
+                tags_data['h1'],
+                tags_data['title'],
+                tags_data['description'],
+                status_code,
+                datetime.now(timezone.utc)
             )
-            new_record = cursor.fetchone()
-            conn.commit()
-            return new_record.id
-
-    def save_url_check(self, url_id, tags_data, status_code):
-        conn = self.connect()
-        with conn.cursor() as cursor:
-            cursor.execute(
-                '''INSERT INTO url_checks (
-                    url_id,
-                    h1,
-                    title,
-                    description,
-                    status_code,
-                    created_at)
-                VALUES (%s, %s, %s, %s, %s, %s)''',
-                (
-                    url_id,
-                    tags_data['h1'],
-                    tags_data['title'],
-                    tags_data['description'],
-                    status_code,
-                    datetime.utcnow()
-                )
-            )
-            conn.commit()
+        )
